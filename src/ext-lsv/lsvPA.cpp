7b0b57c575aa0f83561ce67a9c4e108d57c854c1
qmo1222 <howard19192828@gmail.com>
2020-10-21 19:43:39 +0800

    sort by id and add offset

--- src/ext-lsv/lsvPA.cpp
+++ src/ext-lsv/lsvPA.cpp
@@ -13,17 +13,19 @@ int Lsv_NtkPrintSopUnate(Abc_Ntk_t *pNtk)
     int nFanins;
     Abc_Obj_t *pObj;
     char *pSop, *pCube;
-    int type;
+    int type, id;
     char val;
     int numBinate;
-    string name;
+    Vec_Int_t *vType;
+    Vec_Int_t *punate, *nunate, *binate;
+    char *name;
 
     Abc_NtkForEachNode(pNtk, pObj, i)
     {
         printf("node %s:\n", Abc_ObjName(pObj));
         nFanins = Abc_ObjFaninNum(pObj);
 
-        vector<int> vType(nFanins, 0);
+        vType = Vec_IntStart(nFanins);
 
         pSop = (char *)pObj->pData;
         // check unateness
@@ -32,23 +34,23 @@ int Lsv_NtkPrintSopUnate(Abc_Ntk_t *pNtk)
             numBinate = 0;
             Abc_CubeForEachVar(pCube, val, j)
             {
-                type = vType[j];
+                type = Vec_IntGetEntry(vType, j);
                 if (type == UNKNOWN)
                 {
                     if (val == '0')
-                        vType[j] = NEGUNATE;
+                        Vec_IntWriteEntry(vType, j, NEGUNATE);
                     else if (val == '1')
-                        vType[j] = POSUNATE;
+                        Vec_IntWriteEntry(vType, j, POSUNATE);
                 }
                 else if (type == POSUNATE)
                 {
                     if (val == '0')
-                        vType[j] = BINATE;
+                        Vec_IntWriteEntry(vType, j, BINATE);
                 }
                 else if (type == NEGUNATE)
                 {
                     if (val == '1')
-                        vType[j] = BINATE;
+                        Vec_IntWriteEntry(vType, j, BINATE);
                 }
                 else if (type == BINATE)
                 {
@@ -64,54 +66,79 @@ int Lsv_NtkPrintSopUnate(Abc_Ntk_t *pNtk)
                 break;
         }
 
-        // summarize types of fanins
-        vector<string> punate, nunate, binate;
-        for (j = 0; j < vType.size(); j++)
+        // flip if the sop is offset
+        if (Abc_SopIsComplement(pSop))
         {
-            type = vType[j];
-            name = Abc_ObjName(Abc_ObjFanin(pObj, j));
-            if (type == POSUNATE)
-                punate.push_back(name);
-            else if (type == NEGUNATE)
-                nunate.push_back(name);
-            else if (type == BINATE)
-                binate.push_back(name);
+            Vec_IntForEachEntry(vType, type, j)
+            {
+                if (type == POSUNATE)
+                    Vec_IntWriteEntry(vType, j, NEGUNATE);
+                else if (type == NEGUNATE)
+                    Vec_IntWriteEntry(vType, j, POSUNATE);
+            }
         }
-        sort(punate.begin(), punate.end());
-        sort(nunate.begin(), nunate.end());
-        sort(binate.begin(), binate.end());
+
+        punate = Vec_IntAlloc(nFanins);
+        nunate = Vec_IntAlloc(nFanins);
+        binate = Vec_IntAlloc(nFanins);
+
+        // summarize types of fanins
+        Vec_IntForEachEntry(vType, type, j)
+        {
+            id = Abc_ObjId(Abc_ObjFanin(pObj, j));
+            if (type == POSUNATE)
+                Vec_IntPush(punate, id);
+            else if (type == NEGUNATE)
+                Vec_IntPush(nunate, id);
+            else if (type == BINATE)
+                Vec_IntPush(binate, id);
+            else if (type == UNKNOWN)
+            {
+                Vec_IntPush(punate, id);
+                Vec_IntPush(nunate, id);
+            }
+        }
+
+        Vec_IntSort(punate, 0);
+        Vec_IntSort(nunate, 0);
+        Vec_IntSort(binate, 0);
 
         // print results
-        if (!punate.empty())
+        if (Vec_IntSize(punate) != 0)
         {
             printf("+unate inputs:");
-            for (j = 0; j < punate.size(); j++)
+            Vec_IntForEachEntry(punate, id, j)
             {
-                name = punate[j];
-                printf(" %s", name.c_str());
+                name = Abc_ObjName(Abc_NtkObj(pNtk, id));
+                printf(" %s", name);
             }
             printf("\n");
         }
-        if (!nunate.empty())
+        if (Vec_IntSize(nunate) != 0)
         {
             printf("-unate inputs:");
-            for (j = 0; j < nunate.size(); j++)
+            Vec_IntForEachEntry(nunate, id, j)
             {
-                name = nunate[j];
-                printf(" %s", name.c_str());
+                name = Abc_ObjName(Abc_NtkObj(pNtk, id));
+                printf(" %s", name);
             }
             printf("\n");
         }
-        if (!binate.empty())
+        if (Vec_IntSize(binate) != 0)
         {
             printf("binate inputs:");
-            for (j = 0; j < binate.size(); j++)
+            Vec_IntForEachEntry(binate, id, j)
             {
-                name = binate[j];
-                printf(" %s", name.c_str());
+                name = Abc_ObjName(Abc_NtkObj(pNtk, id));
+                printf(" %s", name);
             }
             printf("\n");
         }
+
+        Vec_IntFree(vType);
+        Vec_IntFree(punate);
+        Vec_IntFree(nunate);
+        Vec_IntFree(binate);
     }
 
     return 0;
