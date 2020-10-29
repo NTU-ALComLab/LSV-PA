d4222041fbde0f446df2d99bea4e4ff1eab5111e
Runnlion <44282456+Runnlion@users.noreply.github.com>
2020-10-29 13:26:33 +0800

    Update lsvCmd.cpp

--- lsvCmd.cpp
+++ lsvCmd.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 #include <string>
 using namespace std;
-static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
-static int Lsv_CommandPrintUnates(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
