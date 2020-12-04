5641bc00b96bf4cca19a8d84668ab550bddcbdb2
rededler <9631218@gmail.com>
2020-12-04 22:21:57 +0800

    add file for pa2

---
+++ src/ext-lsv/lsvPounate.cpp
@@ -0,0 +1,8 @@
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+#include "misc/vec/vec.h"
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
