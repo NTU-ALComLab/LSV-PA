8a68a1f45aa595f5d100318187639345a9c42e43
rededler <9631218@gmail.com>
2020-12-05 14:17:00 +0800

    transform Ntk to Aig transform Aig to Cnf

--- src/ext-lsv/lsvPounate.cpp
+++ src/ext-lsv/lsvPounate.cpp
@@ -1,11 +1,11 @@
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
-#include "misc/vec/vec.h"
+#include "sat/cnf/cnf.h"
+#include "aig/aig/aig.h"
 #include <vector>
 #include <string>
 #include <algorithm>
-using namespace std;
 
 static int Lsv_CommandPrintPounate(Abc_Frame_t* pAbc, int argc, char** argv);
 
@@ -20,6 +20,147 @@ Abc_FrameInitializer_t lsv_print_pounate_initializer = {lsv_print_pounate_init, 
 struct PackageRegistrationManager {
   PackageRegistrationManager() { Abc_FrameAddInitializer(&lsv_print_pounate_initializer); }
 } lsv_print_pounate_PackageRegistrationManager;
+
+/**Function*************************************************************
+
+  Synopsis    [Converts the network from the AIG manager into ABC.]
+
+  Description [Assumes that registers are ordered after PIs/POs.]
+               
+  SideEffects []
+
+  SeeAlso     []
+
+***********************************************************************/
+Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * pNtk, int fExors, int fRegisters )
+{
+    Vec_Ptr_t * vNodes;
+    Aig_Man_t * pMan;
+    Aig_Obj_t * pObjNew;
+    Abc_Obj_t * pObj;
+    int i, nNodes, nDontCares;
+    // make sure the latches follow PIs/POs
+    if ( fRegisters ) 
+    { 
+        assert( Abc_NtkBoxNum(pNtk) == Abc_NtkLatchNum(pNtk) );
+        Abc_NtkForEachCi( pNtk, pObj, i )
+            if ( i < Abc_NtkPiNum(pNtk) )
+            {
+                assert( Abc_ObjIsPi(pObj) );
+                if ( !Abc_ObjIsPi(pObj) )
+                    Abc_Print( 1, "Abc_NtkToDar(): Temporary bug: The PI ordering is wrong!\n" );
+            }
+            else
+                assert( Abc_ObjIsBo(pObj) );
+        Abc_NtkForEachCo( pNtk, pObj, i )
+            if ( i < Abc_NtkPoNum(pNtk) )
+            {
+                assert( Abc_ObjIsPo(pObj) );
+                if ( !Abc_ObjIsPo(pObj) )
+                    Abc_Print( 1, "Abc_NtkToDar(): Temporary bug: The PO ordering is wrong!\n" );
+            }
+            else
+                assert( Abc_ObjIsBi(pObj) );
+        // print warning about initial values
+        nDontCares = 0;
+        Abc_NtkForEachLatch( pNtk, pObj, i )
+            if ( Abc_LatchIsInitDc(pObj) )
+            {
+                Abc_LatchSetInit0(pObj);
+                nDontCares++;
+            }
+        if ( nDontCares )
+        {
+            Abc_Print( 1, "Warning: %d registers in this network have don't-care init values.\n", nDontCares );
+            Abc_Print( 1, "The don't-care are assumed to be 0. The result may not verify.\n" );
+            Abc_Print( 1, "Use command \"print_latch\" to see the init values of registers.\n" );
+            Abc_Print( 1, "Use command \"zero\" to convert or \"init\" to change the values.\n" );
+        }
+    }
+    // create the manager
+    pMan = Aig_ManStart( Abc_NtkNodeNum(pNtk) + 100 );
+    pMan->fCatchExor = fExors;
+    pMan->nConstrs = pNtk->nConstrs;
+    pMan->nBarBufs = pNtk->nBarBufs;
+    pMan->pName = Extra_UtilStrsav( pNtk->pName );
+    pMan->pSpec = Extra_UtilStrsav( pNtk->pSpec );
+    // transfer the pointers to the basic nodes
+    Abc_AigConst1(pNtk)->pCopy = (Abc_Obj_t *)Aig_ManConst1(pMan);
+    Abc_NtkForEachCi( pNtk, pObj, i )
+    {
+        pObj->pCopy = (Abc_Obj_t *)Aig_ObjCreateCi(pMan);
+        // initialize logic level of the CIs
+        ((Aig_Obj_t *)pObj->pCopy)->Level = pObj->Level;
+    }
+
+    // complement the 1-values registers
+    if ( fRegisters ) {
+        Abc_NtkForEachLatch( pNtk, pObj, i )
+            if ( Abc_LatchIsInit1(pObj) )
+                Abc_ObjFanout0(pObj)->pCopy = Abc_ObjNot(Abc_ObjFanout0(pObj)->pCopy);
+    }
+    // perform the conversion of the internal nodes (assumes DFS ordering)
+//    pMan->fAddStrash = 1;
+    vNodes = Abc_NtkDfs( pNtk, 0 );
+    Vec_PtrForEachEntry( Abc_Obj_t *, vNodes, pObj, i )
+//    Abc_NtkForEachNode( pNtk, pObj, i )
+    {
+        pObj->pCopy = (Abc_Obj_t *)Aig_And( pMan, (Aig_Obj_t *)Abc_ObjChild0Copy(pObj), (Aig_Obj_t *)Abc_ObjChild1Copy(pObj) );
+//        Abc_Print( 1, "%d->%d ", pObj->Id, ((Aig_Obj_t *)pObj->pCopy)->Id );
+    }
+    Vec_PtrFree( vNodes );
+    pMan->fAddStrash = 0;
+    // create the POs
+    Abc_NtkForEachCo( pNtk, pObj, i )
+        Aig_ObjCreateCo( pMan, (Aig_Obj_t *)Abc_ObjChild0Copy(pObj) );
+    // complement the 1-valued registers
+    Aig_ManSetRegNum( pMan, Abc_NtkLatchNum(pNtk) );
+    if ( fRegisters )
+        Aig_ManForEachLiSeq( pMan, pObjNew, i )
+            if ( Abc_LatchIsInit1(Abc_ObjFanout0(Abc_NtkCo(pNtk,i))) )
+                pObjNew->pFanin0 = Aig_Not(pObjNew->pFanin0);
+    // remove dangling nodes
+    nNodes = (Abc_NtkGetChoiceNum(pNtk) == 0)? Aig_ManCleanup( pMan ) : 0;
+    if ( !fExors && nNodes )
+        Abc_Print( 1, "Abc_NtkToDar(): Unexpected %d dangling nodes when converting to AIG!\n", nNodes );
+//Aig_ManDumpVerilog( pMan, "test.v" );
+    // save the number of registers
+    if ( fRegisters )
+    {
+        Aig_ManSetRegNum( pMan, Abc_NtkLatchNum(pNtk) );
+        pMan->vFlopNums = Vec_IntStartNatural( pMan->nRegs );
+//        pMan->vFlopNums = NULL;
+//        pMan->vOnehots = Abc_NtkConverLatchNamesIntoNumbers( pNtk );
+        if ( pNtk->vOnehots )
+            pMan->vOnehots = (Vec_Ptr_t *)Vec_VecDupInt( (Vec_Vec_t *)pNtk->vOnehots );
+    }
+    if ( !Aig_ManCheck( pMan ) )
+    {
+        Abc_Print( 1, "Abc_NtkToDar: AIG check has failed.\n" );
+        Aig_ManStop( pMan );
+        return NULL;
+    }
+    return pMan;
+}
+
+void Lsv_NtkPrintPounate(Abc_Ntk_t* pNtk) {
+  assert( Abc_NtkIsStrash(pNtk) );
+  assert( Abc_NtkLatchNum(pNtk) == 0 );
+  //extern Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * _pNtk, int _fExors, int _fRegisters );
+  Abc_Obj_t* pObj;
+  int i;
+  Aig_Man_t * pMan;
+  Cnf_Dat_t * pCnf;
+  
+  pMan = Abc_NtkToDar( pNtk, 0, 0);
+  pCnf = Cnf_Derive(pMan, 0);
+  Abc_NtkForEachPo(pNtk, pObj, i) {
+    if (true) {
+       printf("node %s:\n", Abc_ObjName(pObj));
+    }
+  }
+}
+
 
 int Lsv_CommandPrintPounate(Abc_Frame_t* pAbc, int argc, char** argv) {
   //printf("Lsv_CommandPrintPounate\n");
@@ -49,5 +190,4 @@ usage:
   return 1;
 }
 
-void Lsv_NtkPrintPounate(Abc_Ntk_t* pNtk) {
-}
+
