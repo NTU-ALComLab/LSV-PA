ed9505f1266142153fce36b84adf4117ba9e8581
ian861226 <ian861226@gmail.com>
2020-12-08 11:09:52 +0800

    Add header

---
+++ src/ext-lsv/pounate.h
@@ -0,0 +1,8 @@
+#include "base/abc/abc.h"
+#include "aig/aig/aig.h"
+
+ABC_NAMESPACE_HEADER_START
+
+extern Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * pPo_ConeNtk, int , int );
+
+ABC_NAMESPACE_HEADER_END
