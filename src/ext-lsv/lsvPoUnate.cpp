a2958e5ef29d4aa2cc76e04404234fce4dc950d1
Luo-Yun-Rong <b06901073@ntu.edu.tw>
2020-12-09 18:57:37 +0800

    create cone

--- src/ext-lsv/lsvPoUnate.cpp
+++ src/ext-lsv/lsvPoUnate.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <string.h>
 
 using namespace std;
 
@@ -48,70 +49,75 @@ struct PackageRegistrationManager {
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
-void record_pio_var(Cnf_Dat_t *pCnf, vector<int> &vpi, vector<int> &vpo){
-    Aig_Obj_t * pObj; int i=0;
+void record_pio_var(Cnf_Dat_t *pCnf, vector<int>& vpis, int& vpo){
+    
+    Aig_Obj_t * pObj; int i=0, k=0;
+    //Pi
     Aig_ManForEachCi( pCnf->pMan, pObj, i )
     {
-        if ( pCnf->pVarNums[pObj->Id] >= 0 )
-            vpi.push_back( pCnf->pVarNums[pObj->Id] );
+        assert( pCnf->pVarNums[pObj->Id] >= 0 );
+        while(vpis[k] == -2) k++;
+        assert(vpis[k] == -1);
+        vpis[k] = pCnf->pVarNums[pObj->Id] ;
+        k++;
     }
-    Aig_ManForEachCo( pCnf->pMan, pObj, i )
-    {
-        if ( pCnf->pVarNums[pObj->Id] >= 0 )
-            vpo.push_back( pCnf->pVarNums[pObj->Id] );    
-    }
+    //Po
+    assert( Aig_ManCoNum(pCnf->pMan) == 1 );
+    pObj = Aig_ManCo( pCnf->pMan, 0 ); 
+    assert( pCnf->pVarNums[pObj->Id] >= 0 );
+    vpo = pCnf->pVarNums[pObj->Id] ;    
 }
 
-void connect_pi(sat_solver *pSat, vector<int>& posCofPiList, vector<int>& negCofPiList, vector<int>& disableList){
-    
+void connect_pi(sat_solver *pSat, vector<int>& posCofPiVars, int VarShift, vector<int>& ForceEqVars ){
+      
     lit Lits[3];
+    assert(ForceEqVars.size()== posCofPiVars.size());
+    for(int i=0; i< posCofPiVars.size(); ++i){
+        if(posCofPiVars[i] == -2) 
+            assert(ForceEqVars[i] == -2);
+        else{
+            assert(ForceEqVars[i] != -2 && posCofPiVars[i]>=0 );
+            ForceEqVars[i] = sat_solver_addvar(pSat); // ax
 
-    for(int i=0; i< posCofPiList.size(); ++i){
+            // (~ax + x <-> x')
+            Lits[0] = toLitCond( posCofPiVars[i], 0 ); // x
+            Lits[1] = toLitCond( posCofPiVars[i]+VarShift, 1 ); // ~x'
+            Lits[2] = toLitCond( ForceEqVars[i],  1);  // ~ax
+            sat_solver_addclause( (sat_solver *)pSat, Lits, Lits + 3 );          
 
-        disableList.push_back(sat_solver_addvar(pSat)); // ax
-
-        // (~ax + x <-> x')
-        Lits[0] = toLitCond( posCofPiList[i], 0 ); // x
-        Lits[1] = toLitCond( negCofPiList[i], 1 ); // ~x'
-        Lits[2] = toLitCond( disableList[i],  1);  // ~ax
-        sat_solver_addclause( (sat_solver *)pSat, Lits, Lits + 3 );          
-
-        Lits[0] = toLitCond( posCofPiList[i], 1 ); // ~x
-        Lits[1] = toLitCond( negCofPiList[i], 0 ); // x'
-        Lits[2] = toLitCond( disableList[i],  1);  // ~ax
-        sat_solver_addclause( (sat_solver *)pSat, Lits, Lits + 3 );            
+            Lits[0] = toLitCond( posCofPiVars[i], 1 ); // ~x
+            Lits[1] = toLitCond( posCofPiVars[i]+VarShift, 0 ); // x'
+            Lits[2] = toLitCond( ForceEqVars[i], 1);  // ~ax
+            sat_solver_addclause( (sat_solver *)pSat, Lits, Lits + 3 );            
+        }
     } 
-    
 }
 
-sat_solver * construct_CNF(Aig_Man_t * pMan, vector<int>& posCofPoList, vector<int>& negCofPoList, 
-                   vector<int>& posCofPiList, vector<int>& negCofPiList, vector<int>& disableList ){
-    
+sat_solver * construct_CNF(Aig_Man_t * pMan, int& posCofPoVar, vector<int>& posCofPiVars, 
+                           int& negCofVarShift, vector<int>& ForceEqVars){
+
     pMan->pData = NULL;
     
     // derive CNF for positive cofactor circuit
     Cnf_Dat_t * pCnf= Cnf_Derive( pMan, Aig_ManCoNum(pMan) );
 
     //record Pi var num and Po var num for positive cofactor circuit
-    record_pio_var(pCnf, posCofPiList, posCofPoList);
-
+    record_pio_var(pCnf, posCofPiVars, posCofPoVar);
+    
     // write cnf into SAT solver
     sat_solver *pSat = (sat_solver *)Cnf_DataWriteIntoSolver( pCnf, 1, 0 );
 
     // rename CNF variable for negative cofactor circuit
+    assert(negCofVarShift==0);
+    negCofVarShift = pCnf->nVars;
     Cnf_DataLift( pCnf, pCnf->nVars );
     
-    //record Pi var num and Po var num for negative cofactor circuit
-    record_pio_var(pCnf, negCofPiList, negCofPoList);
-    assert(posCofPiList.size()==negCofPiList.size()); 
-    assert(posCofPoList.size()==negCofPoList.size());
-
     // write the variable-renamed cnf into SAT solver
     for ( int i = 0; i < pCnf->nClauses; ++i )
         sat_solver_addclause( pSat, pCnf->pClauses[i], pCnf->pClauses[i+1]);
     
     //connect the two circuits
-    connect_pi(pSat, posCofPiList, negCofPiList, disableList);
+    connect_pi(pSat, posCofPiVars, negCofVarShift, ForceEqVars);
 
     Cnf_DataFree( pCnf );
     
@@ -119,42 +125,63 @@ sat_solver * construct_CNF(Aig_Man_t * pMan, vector<int>& posCofPoList, vector<i
 }
 
 
-void resetUnateVec(vector<int>& UnateVec){
+void resetVec(vector<int>& Vec, int v){
     //reset the content of UnateVec to binary value:11
-    fill(UnateVec.begin(), UnateVec.end(),3);     
+    fill(Vec.begin(), Vec.end(),v);     
 }
 
-void test_unate( int i, sat_solver *pSat, vector<int>& UnateVec, int posPo, int negPo, int isNeg,
+void test_unate( int i, sat_solver *pSat, vector<int>& UnateVec, int posCofPoVar, int VarShift, int isNeg,
                  lit* assumpList, int assumpSize ){
         
         //check for positive unate, F~x -> Fx,  check F~x ^ ~Fx unsat
         //check for negative unate  Fx -> F~x,  check Fx ^ ~F~x unsat
-        assumpList[assumpSize-2] = toLitCond(posPo, 1^isNeg);
-        assumpList[assumpSize-1] = toLitCond(negPo, 0^isNeg); 
+        assumpList[assumpSize-2] = toLitCond( posCofPoVar, 1^isNeg);
+        assumpList[assumpSize-1] = toLitCond( posCofPoVar+VarShift, 0^isNeg); 
 
         int status = sat_solver_solve( pSat,assumpList, assumpList+assumpSize, (ABC_INT64_T)0, (ABC_INT64_T)0, (ABC_INT64_T)0, (ABC_INT64_T)0 );
         if ( status == l_False ) //unsat
             UnateVec[i] &= (1 << isNeg); 
 }
 
-void setUnateVec( sat_solver *pSat, vector<int>& UnateVec, int posPo, int negPo, vector<int>& posCofPiList, 
-                  vector<int>& negCofPiList, vector<int>& disableList, lit* assumpList, int assumpSize ){
-    
-    for(int i=0;i< posCofPiList.size(); ++i) 
-        assumpList[i] = toLitCond(disableList[i], 0); // ax=1 force x<->x'
- 
-    for(int i=0; i< posCofPiList.size(); ++i){
-        //set ax
-        assumpList[i] = toLitCond(disableList[i], 1); // ax=0 
-        assumpList[assumpSize-4] = toLitCond(posCofPiList[i],0); // x =1
-        assumpList[assumpSize-3] = toLitCond(negCofPiList[i],1); // x'=0
-        
-        //test positive unate
-        test_unate( i, pSat, UnateVec, posPo, negPo, 0 , assumpList, assumpSize );         
-        //test negative unate
-        test_unate( i, pSat, UnateVec, posPo, negPo, 1 , assumpList, assumpSize ); 
-        //reset ax
-        assumpList[i] = toLitCond(disableList[i], 0); // ax=1 force x<->x'
+int init_assump( vector<int>& posCofPiVars, vector<int>& ForceEqVars, lit* assumpList){
+
+    int k=0; 
+    for(int i=0;i< posCofPiVars.size(); ++i){ 
+        if(posCofPiVars[i]!=-2){
+            assert( posCofPiVars[i]>=0 && ForceEqVars[i] >=0);
+            assumpList[k] = toLitCond(ForceEqVars[i], 0); // ax=1 force x<->x'
+            k++;
+        }
+        else assert(ForceEqVars[i] == -2);
+    }
+    return k+4;
+
+}
+
+void setUnateVec( sat_solver *pSat, vector<int>& UnateVec, int posCofPoVar, vector<int>& posCofPiVars, 
+                  int VarShift, vector<int>& ForceEqVars, lit* assumpList){
+   
+    //set assumption list and assumption size
+    int assumpSize = init_assump( posCofPiVars, ForceEqVars, assumpList);
+
+    int k=0;
+    for(int i=0; i< posCofPiVars.size(); ++i){
+        if(posCofPiVars[i]!=-2 ){
+            //set ax
+            assumpList[k] = toLitCond( ForceEqVars[i], 1); // ax=0 
+            assumpList[assumpSize-4] = toLitCond( posCofPiVars[i], 0); // x =1
+            assumpList[assumpSize-3] = toLitCond( posCofPiVars[i]+VarShift, 1); // x'=0
+            
+            //test positive unate
+            test_unate( i, pSat, UnateVec, posCofPoVar, VarShift, 0 , assumpList, assumpSize );         
+            //test negative unate
+            test_unate( i, pSat, UnateVec, posCofPoVar, VarShift, 1 , assumpList, assumpSize ); 
+            //reset ax
+            assumpList[k] = toLitCond(ForceEqVars[i], 0); // ax=1 force x<->x'
+            k++;
+        }
+        else
+            UnateVec [i] = 0; //both positive and negative unate
 
     }
 }
@@ -174,24 +201,44 @@ void print_unate( Abc_Ntk_t * pNtk, vector<int>& UnateVec, int n){
     if(occur) printf("\n");
 }
 
+void set_nonused_pi( Abc_Ntk_t * pNtk, Abc_Ntk_t * pNtkPo, vector<int>& posCofPiVars, vector<int>& ForceEqVars){
+
+    int j=0, k=0;
+    int piNum = Abc_NtkPiNum(pNtk);
+    Abc_Obj_t *pNodePi;
+    Abc_NtkForEachPi(pNtkPo, pNodePi, j ){
+        assert( k < piNum );
+        while( strcmp(Abc_ObjName( Abc_NtkPi(pNtk,k) ), Abc_ObjName( pNodePi )) !=0 ){
+               //not equal
+               posCofPiVars[k]= -2; //set not used flag 
+               ForceEqVars[k]= -2;
+               k++;
+               assert( k< piNum );
+        }
+        k++;
+    }
+    for( ; k< piNum ; ++k){
+        posCofPiVars[k]= -2; //set not used flag 
+        ForceEqVars[k]= -2;
+    }
+}
+
 extern "C"{
 void Lsv_NtkPrintPoUnate(Abc_Ntk_t* pNtk) {
     assert( Abc_NtkIsStrash(pNtk) );
     assert( Abc_NtkLatchNum(pNtk) == 0 );
 
-    //create Aig manager
     extern  Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * pNtk, int fExors, int fRegisters ); 
-    Aig_Man_t * pMan = Abc_NtkToDar( pNtk, 0, 0 );
+    int piNum = Abc_NtkPiNum(pNtk);
 
-    //construct cnf 
-    sat_solver * pSat;
-    vector<int> posCofPoList, negCofPoList, posCofPiList, negCofPiList, disableList; //store cnf var num 
-    pSat = construct_CNF(pMan, posCofPoList, negCofPoList, posCofPiList, negCofPiList, disableList);
+    //store cnf variable num
+    vector<int>  posCofPiVars(piNum, -1), ForceEqVars(piNum,-1);  
+    int posCofPoVar=0, negCofVarShift=0;
 
     //assumption list
-    int assumpSize = posCofPiList.size()+4;
-    lit* assumpList = new lit [ assumpSize ];
+    lit* assumpList = new lit [ piNum+4 ];
 
+    //represent the unateness of each pi
     vector<int> UnateVec(Abc_NtkPiNum(pNtk),3);
     // Encode:
     // 11: binate 
@@ -199,17 +246,24 @@ void Lsv_NtkPrintPoUnate(Abc_Ntk_t* pNtk) {
     // 01: positive unate
     // 10: negative unate
 
-    //sort by pair( Id, Abc_Obj_t* )
-    //IdObjQueue PosQueue, NegQueue, BiQueue; 
-    
-    Abc_Obj_t* pObj; int i;
+    Abc_Obj_t *pObj; int i; 
     Abc_NtkForEachPo(pNtk, pObj, i) {
-        
+
+        Abc_Ntk_t * pNtkPo = Abc_NtkCreateCone( pNtk, Abc_ObjFanin0(pObj), Abc_ObjName(pObj), 0 );
+        if ( Abc_ObjFaninC0(pObj) )
+           Abc_ObjXorFaninC( Abc_NtkPo(pNtkPo, 0), 0 );
+        Aig_Man_t * pMan = Abc_NtkToDar( pNtkPo, 0, 0 );
+        assert( Aig_ManCoNum(pMan) == 1 );
+
+        //set the nonused pi entry of posCofPiVars and ForceEqVar
+        set_nonused_pi(pNtk, pNtkPo, posCofPiVars,ForceEqVars);
+
+        //construct cnf 
+        sat_solver * pSat = construct_CNF(pMan, posCofPoVar, posCofPiVars, negCofVarShift, ForceEqVars);
+
         //determine the po's unateness with respect to all pis
-        resetUnateVec(UnateVec); 
-        setUnateVec( pSat, UnateVec, posCofPoList[i], negCofPoList[i] , posCofPiList, 
-                     negCofPiList,disableList, assumpList, assumpSize );
-        
+        setUnateVec( pSat, UnateVec, posCofPoVar, posCofPiVars, negCofVarShift, ForceEqVars, assumpList );
+
         printf("node %s:\n", Abc_ObjName(pObj));
         //print pos unate
         print_unate( pNtk, UnateVec, 1);
@@ -217,12 +271,16 @@ void Lsv_NtkPrintPoUnate(Abc_Ntk_t* pNtk) {
         print_unate( pNtk, UnateVec, 2);
         //print binate
         print_unate( pNtk, UnateVec, 3);
-        
-    }
 
-    delete [] assumpList;
-    sat_solver_delete( pSat );
-    Aig_ManStop( pMan );
+        //reset
+        posCofPoVar=0; negCofVarShift=0;
+        resetVec(UnateVec, 3);
+        resetVec(posCofPiVars,-1);
+        resetVec(ForceEqVars, -1);
+        Abc_NtkDelete( pNtkPo );
+        Aig_ManStop( pMan );
+        sat_solver_delete( pSat );
+    }
 }
 }
 
