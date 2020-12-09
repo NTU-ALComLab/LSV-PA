5c1697e3e5577a0de104c45055a1c4db1eb7446c
Davinais <david870620@gmail.com>
2020-12-09 18:49:05 +0800

    Use NtkCreateCone to speed up lsv_print_pounate
    Since put CNF of the entire circuit into the SAT solver would be slowly,
    create a cone for each PO and solve it instead

--- src/ext-lsv/lsvCmdPrintPOUnate.cpp
+++ src/ext-lsv/lsvCmdPrintPOUnate.cpp
@@ -27,13 +27,27 @@ const int LSV_NOUNATENESS = -1,
 
 static const char *UNATE_NAME[LSV_UNATENESS_NUM] = { "+unate", "-unate", "binate" };
 
+static void __Lsv_NtkPrintPOUnate_Cone(Abc_Ntk_t* pConeNtk, Abc_Ntk_t* pParentNtk, int fCompl);
+
 void Lsv_NtkPrintPOUnate(Abc_Ntk_t* pNtk) {
-    Aig_Man_t *pMan = Abc_NtkToDar(pNtk, 0, 0);
+    int i = 0;
+    Abc_Obj_t *pPO;
+    Abc_NtkForEachPo(pNtk, pPO, i) {
+        __Lsv_NtkPrintPOUnate_Cone(
+            Abc_NtkCreateCone(pNtk, Abc_ObjFanin0(pPO), Abc_ObjName(pPO), 0),
+            pNtk,
+            Abc_ObjFaninC0(pPO)
+        );
+    }
+}
+
+static void __Lsv_NtkPrintPOUnate_Cone(Abc_Ntk_t* pConeNtk, Abc_Ntk_t* pParentNtk, int fCompl) {
+    Aig_Man_t *pMan = Abc_NtkToDar(pConeNtk, 0, 0);
     Cnf_Dat_t *pCnf = Cnf_Derive(pMan, Aig_ManCoNum(pMan));
     int i = 0;
     int j = 0;
     int nVars = pCnf->nVars;
-    int nPI = Abc_NtkPiNum(pNtk);
+    int nPI = Abc_NtkPiNum(pConeNtk);
 
     Cnf_Dat_t *pCnf_negcf = Cnf_DataDup(pCnf);
     Cnf_DataLift(pCnf_negcf, nVars);
@@ -42,10 +56,10 @@ void Lsv_NtkPrintPOUnate(Abc_Ntk_t* pNtk) {
     pSat = (sat_solver *)Cnf_DataWriteIntoSolverInt(pSat, pCnf_negcf, 1, 0);
 
     Aig_Obj_t *pPO;
-    Abc_Obj_t *pPI;
+    Abc_Obj_t *pPI_parent, *pPI_cone;
     sat_solver_setnvars(pSat, pSat->size + nPI);
-    Abc_NtkForEachPi(pNtk, pPI, i) {
-        int pi_var = pCnf->pVarNums[pPI->Id];
+    Abc_NtkForEachPi(pConeNtk, pPI_cone, i) {
+        int pi_var = pCnf->pVarNums[pPI_cone->Id];
         sat_solver_add_buffer_enable(pSat, pi_var, pi_var + nVars, i + nVars*2, 0);
     }
 
@@ -57,56 +71,64 @@ void Lsv_NtkPrintPOUnate(Abc_Ntk_t* pNtk) {
 
     vector<Abc_Obj_t> unateness_vecs[LSV_UNATENESS_NUM];
     char unateness = 0;
+    int pi_index = 0;
     Aig_ManForEachCo(pMan, pPO, i) {
         int po_var = pCnf->pVarNums[pPO->Id];
-        Abc_NtkForEachPi(pNtk, pPI, j) {
-            int pi_var = pCnf->pVarNums[pPI->Id];
+        Abc_NtkForEachPi(pParentNtk, pPI_parent, j) {
             unateness = 0;
+            pPI_cone = Abc_NtkFindCi(pConeNtk, Abc_ObjName(pPI_parent));
+            if (pPI_cone) {
+                int pi_var = pCnf->pVarNums[pPI_cone->Id];
+                // Set current PI constraint with X_1 = 1, X_2 = 0 to do
+                // positive and negative cofactor, respectively
+                constraint[pi_index] = toLitCond(pi_index + nVars*2, 1);
+                constraint[nPI] = toLitCond(pi_var, 0);
+                constraint[nPI+1] = toLitCond(pi_var + nVars, 1);
 
-            // Set current PI constraint with X_1 = 1, X_2 = 0 to do
-            // positive and negative cofactor, respectively
-            constraint[j] = toLitCond(j + nVars*2, 1);
-            constraint[nPI] = toLitCond(pi_var, 0);
-            constraint[nPI+1] = toLitCond(pi_var + nVars, 1);
+                // Positive Unate Check
+                constraint[nPI+2] = toLitCond(po_var, 1);
+                constraint[nPI+3] = toLitCond(po_var + nVars, 0);
+                int ret = sat_solver_solve(pSat, constraint, (constraint + constraint_len), 0, 0, 0, 0);
+                unateness |= (ret == l_True)
+                    ? (!fCompl ? LSV_POSITIVE_UNATE_CHECK_FAIL : LSV_NEGATIVE_UNATE_CHECK_FAIL)
+                    : 0;
 
-            // Positive Unate Check
-            constraint[nPI+2] = toLitCond(po_var, 1);
-            constraint[nPI+3] = toLitCond(po_var + nVars, 0);
-            int ret = sat_solver_solve(pSat, constraint, (constraint + constraint_len), 0, 0, 0, 0);
-            unateness |= (ret == l_True) ? LSV_POSITIVE_UNATE_CHECK_FAIL : 0;
-
-            // Negative Unate Check
-            constraint[nPI+2] = toLitCond(po_var, 0);
-            constraint[nPI+3] = toLitCond(po_var + nVars, 1);
-            ret = sat_solver_solve(pSat, constraint, (constraint + constraint_len), 0, 0, 0, 0);
-            unateness |= (ret == l_True) ? LSV_NEGATIVE_UNATE_CHECK_FAIL : 0;
-            
-            // Reset the current PI constraint to X_1 = X_2
-            constraint[j] = toLitCond(j + nVars*2, 0);
+                // Negative Unate Check
+                constraint[nPI+2] = toLitCond(po_var, 0);
+                constraint[nPI+3] = toLitCond(po_var + nVars, 1);
+                ret = sat_solver_solve(pSat, constraint, (constraint + constraint_len), 0, 0, 0, 0);
+                unateness |= (ret == l_True)
+                    ? (!fCompl ? LSV_NEGATIVE_UNATE_CHECK_FAIL : LSV_POSITIVE_UNATE_CHECK_FAIL)
+                    : 0;
+                
+                // Reset the current PI constraint to X_1 = X_2
+                constraint[pi_index] = toLitCond(pi_index + nVars*2, 0);
+                pi_index++;
+            }
 
             switch (unateness)
             {
                 case LSV_POSITIVE_UNATE_FLAG: {
-                    unateness_vecs[LSV_POSITIVE_UNATE].push_back(*pPI);
+                    unateness_vecs[LSV_POSITIVE_UNATE].push_back(*pPI_parent);
                     break;
                 }
                 case LSV_NEGATIVE_UNATE_FLAG: {
-                    unateness_vecs[LSV_NEGATIVE_UNATE].push_back(*pPI);
+                    unateness_vecs[LSV_NEGATIVE_UNATE].push_back(*pPI_parent);
                     break;
                 }
                 case LSV_BINATE_FLAG: {
-                    unateness_vecs[LSV_BINATE].push_back(*pPI);
+                    unateness_vecs[LSV_BINATE].push_back(*pPI_parent);
                     break;
                 }
                 default: {
-                    unateness_vecs[LSV_POSITIVE_UNATE].push_back(*pPI);
-                    unateness_vecs[LSV_NEGATIVE_UNATE].push_back(*pPI);
+                    unateness_vecs[LSV_POSITIVE_UNATE].push_back(*pPI_parent);
+                    unateness_vecs[LSV_NEGATIVE_UNATE].push_back(*pPI_parent);
                     break;
                 }
             }
         }
 
-        printf("node %s:\n", Abc_ObjName(Abc_NtkPo(pNtk, i)));
+        printf("node %s:\n", Abc_ObjName(Abc_NtkPo(pConeNtk, i)));
         for(int p = 0; p < LSV_UNATENESS_NUM; p++) {
             if (!unateness_vecs[p].empty()) {
                 sort(unateness_vecs[p].begin(), unateness_vecs[p].end(), Lsv_CmpAbcObjId);
