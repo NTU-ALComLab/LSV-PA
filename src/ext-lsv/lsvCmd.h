d03a6850f67967acc42d0674bd04e2b0f7e3b619
baby <b06901081@ntu.edu.tw>
2020-12-10 11:45:20 +0800

    upload pa2

---
+++ src/ext-lsv/lsvCmd.h
@@ -0,0 +1,18 @@
+#ifndef LSVCMD_H
+#define LSVCMD_H
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+enum unate_type {
+  positive = 0,
+  negetive,
+  unates_size
+};
+
+#endif
