f29c87fc0edcb2fb464980b072a10ec2ea912928
HHHUUUGGGOOO <hugo551565@gmail.com>
2021-12-20 17:21:23 +0800

    fuck debug

--- src/ext-lsv/lsv_or_bidec.cpp
+++ src/ext-lsv/lsv_or_bidec.cpp
@@ -104,6 +104,9 @@ void Lsv_NtkOrBidec(Abc_Ntk_t* pNtk)
     // debug
     pSat->fPrintClause = true;
 
+    // debug 
+    for (int i = 0 ; i < count_used ; ++i) { sat_solver_addclause(pSat, pCNF->pClauses[i], pCNF->pClauses[i+1]); }
+
         // Obtain "VarShift" by extracting the max varnum() in CNF
     // int VarShift = 0, X_VarNum = pCNF->nVars
     // int VarShift = 0;
@@ -212,8 +215,8 @@ void Lsv_NtkOrBidec(Abc_Ntk_t* pNtk)
     for (int i = 0 ; i < count_used ; ++i)
     {
       // ?????? sat_solver_addvar return "s->size-1"
-      control_a.push_back(sat_solver_addvar(pSat)+1);
-      control_b.push_back(sat_solver_addvar(pSat)+1);
+      control_a.push_back(sat_solver_addvar(pSat));
+      control_b.push_back(sat_solver_addvar(pSat));
     }
     for (int i = 0 ; i < count_used ; ++i)
     {
