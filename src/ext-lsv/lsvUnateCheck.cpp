4ed6c1493767f12dc9fcd24016240fcd39f61e41
Wesley Chen <1011.5131001w@gmail.com>
2025-11-25 23:12:15 +0800

    ADD: split files for PA2

---
+++ src/ext-lsv/lsvUnateCheck.cpp
@@ -0,0 +1,19 @@
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+
+// STL related
+#include <vector>
+#include <set>
+#include <map>
+
+
+int Lsv_CommandUnateBDD(Abc_Frame_t* pAbc, int argc, char** argv){
+    printf("LSV Command lsv_unate_bdd is called.\n");
+    return 0;
+}
+
+int Lsv_CommandUnateSAT(Abc_Frame_t* pAbc, int argc, char** argv){
+    printf("LSV Command lsv_unate_sat is called.\n");
+    return 0;
+}
