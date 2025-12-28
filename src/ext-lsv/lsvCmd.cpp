341aa8842c148dc1920144116945d9cb26c410d4
allen1236 <black49483@gmail.com>
2025-12-28 14:03:01 +0800

    Revert "Merge pull request #929 from Victor-huang1123/master"
    This reverts commit ea3ac00d4ed5ea27eb31601f5125f41f3373a3be, reversing
    changes made to 2d10d818072b87d668e5c60f83552c05bd59c86b.

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,18 +1,11 @@
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <sstream>
-#include <iterator>
 
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
-static int Lsv_CommandPrintMoCut(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
-  Cmd_CommandAdd(pAbc, "LSV", "lsv_printmocut", Lsv_CommandPrintMoCut, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -40,82 +33,6 @@ void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
   }
 }
 
-static std::string CutSig(const std::vector<int>& cut) {
-  std::ostringstream oss;
-  for (size_t i = 0; i < cut.size(); ++i) {
-    if (i) oss << ' ';
-    oss << cut[i];
-  }
-  return oss.str();
-}
-
-static bool IsSubset(const std::vector<int>& a, const std::vector<int>& b) {
-  size_t i = 0, j = 0;
-  while (i < a.size() && j < b.size()) {
-    if (a[i] == b[j]) { ++i; ++j; }
-    else if (a[i] > b[j]) { ++j; }
-    else return false;
-  }
-  return i == a.size();
-}
-
-static void KeepIrredundant(std::vector<std::vector<int>>& cuts) {
-  std::sort(cuts.begin(), cuts.end());
-  cuts.erase(std::unique(cuts.begin(), cuts.end()), cuts.end());
-  std::vector<char> rem(cuts.size(), 0);
-  for (size_t i = 0; i < cuts.size(); ++i) {
-    for (size_t j = 0; j < cuts.size(); ++j) {
-      if (i == j) continue;
-      if (cuts[j].size() < cuts[i].size() && IsSubset(cuts[j], cuts[i])) { rem[i] = 1; break; }
-    }
-  }
-  std::vector<std::vector<int>> out; out.reserve(cuts.size());
-  for (size_t i = 0; i < cuts.size(); ++i) if (!rem[i]) out.push_back(cuts[i]);
-  cuts.swap(out);
-}
-
-static std::vector<std::vector<int>> MergeCuts(const std::vector<std::vector<int>>& a,
-                                               const std::vector<std::vector<int>>& b,
-                                               int k) {
-  std::vector<std::vector<int>> res;
-  for (const auto& ca : a) {
-    for (const auto& cb : b) {
-      std::vector<int> u;
-      std::merge(ca.begin(), ca.end(), cb.begin(), cb.end(), std::back_inserter(u));
-      u.erase(std::unique(u.begin(), u.end()), u.end());
-      if ((int)u.size() <= k) res.push_back(u);
-    }
-  }
-  return res;
-}
-
-static void EnumerateCuts(Abc_Ntk_t* pNtk, int k, std::vector<std::vector<std::vector<int>>>& vCuts) {
-  int nObjs = Abc_NtkObjNum(pNtk);
-  vCuts.assign(nObjs, {});
-  Abc_Obj_t* pObj;
-  int i;
-  Abc_NtkForEachPi(pNtk, pObj, i) {
-    int id = Abc_ObjId(pObj);
-    vCuts[id] = { { id } };
-  }
-  Abc_NtkForEachNode(pNtk, pObj, i) {
-    int id = Abc_ObjId(pObj);
-    Abc_Obj_t* pF0 = Abc_ObjFanin0(pObj);
-    Abc_Obj_t* pF1 = Abc_ObjFanin1(pObj);
-    int id0 = Abc_ObjId(pF0);
-    int id1 = Abc_ObjId(pF1);
-    const auto& c0 = vCuts[id0];
-    const auto& c1 = vCuts[id1];
-    std::vector<std::vector<int>> cuts;
-    cuts.push_back({ id });
-    auto comb = MergeCuts(c0, c1, k);
-    cuts.insert(cuts.end(), comb.begin(), comb.end());
-    for (auto& c : cuts) std::sort(c.begin(), c.end());
-    KeepIrredundant(cuts);
-    vCuts[id] = cuts;
-  }
-}
-
 int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
   Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
   int c;
@@ -140,57 +57,4 @@ usage:
   Abc_Print(-2, "\t        prints the nodes in the network\n");
   Abc_Print(-2, "\t-h    : print the command usage\n");
   return 1;
-}
-
-static int Lsv_CommandPrintMoCut(Abc_Frame_t* pAbc, int argc, char** argv) {
-  if (argc != 3) {
-    Abc_Print(-1, "Usage: lsv_printmocut <k> <l>\n");
-    return 1;
-  }
-  int k = atoi(argv[1]);
-  int l = atoi(argv[2]);
-  if (k < 3 || k > 6 || l < 1 || l > 4) {
-    Abc_Print(-1, "Error: k must be in [3, 6] and l must be in [1, 4].\n");
-    return 1;
-  }
-  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
-  if (!pNtk) {
-    Abc_Print(-1, "Empty network.\n");
-    return 1;
-  }
-  std::vector<std::vector<std::vector<int>>> vCuts;
-  EnumerateCuts(pNtk, k, vCuts);
-  std::unordered_map<std::string, std::vector<int>> cut2outs;
-  Abc_Obj_t* pObj;
-  int i;
-  // Include PIs as output nodes sharing cuts
-  Abc_NtkForEachPi(pNtk, pObj, i) {
-    int id = Abc_ObjId(pObj);
-    for (const auto& cut : vCuts[id]) {
-      if ((int)cut.size() > k) continue;
-      auto sig = CutSig(cut);
-      cut2outs[sig].push_back(id);
-    }
-  }
-  // AND nodes as output nodes
-  Abc_NtkForEachNode(pNtk, pObj, i) {
-    int id = Abc_ObjId(pObj);
-    for (const auto& cut : vCuts[id]) {
-      if ((int)cut.size() > k) continue;
-      auto sig = CutSig(cut);
-      cut2outs[sig].push_back(id);
-    }
-  }
-  for (auto& kv : cut2outs) {
-    auto& outs = kv.second;
-    std::sort(outs.begin(), outs.end());
-    outs.erase(std::unique(outs.begin(), outs.end()), outs.end());
-    if ((int)outs.size() < l) continue;
-    printf("%s :", kv.first.c_str());
-    for (size_t j = 0; j < outs.size(); ++j) {
-      printf(" %d", outs[j]);
-    }
-    printf("\n");
-  }
-  return 0;
 }
